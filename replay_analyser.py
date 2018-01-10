bbe30497cce9338ba6d98366c91b3f5a680e9429
Baldrickk <31375581+Baldrickk@users.noreply.github.com>
2018-01-10 01:19:40 +0000

    minor refactor. Caching is all done in one function that does just that.

--- replay_analyser.py
+++ replay_analyser.py
@@ -207,6 +207,14 @@ def get_player_ratings(id_set, application_id):
   #  print(f'\r{id_list}'.ljust(1000), end = "")
     player_ratings_by_id_to_cache(id_list, application_id)
 
+def cache_players_from_replays()
+  init_player_cache()
+  teams, player_names_to_stat, player_ids_to_stat = get_teams_from_replays(directory)
+  get_player_ids(player_names_to_stat, player_ids_to_stat, application_id)
+  get_player_ratings(player_ids_to_stat, application_id)
+  cache_handle.close()
+  write_clean_cache()
+
 def main():
   if not len(sys.argv) > 1:
     print ('need a directory name')
@@ -218,12 +226,7 @@ def main():
   application_id = sys.argv[2]
   print(f'directory = {directory}\nappID = {application_id}')
 
-  init_player_cache()
-  teams, player_names_to_stat, player_ids_to_stat = get_teams_from_replays(directory)
-  get_player_ids(player_names_to_stat, player_ids_to_stat, application_id)
-  get_player_ratings(player_ids_to_stat, application_id)
-  cache_handle.close()
-  write_clean_cache()
+  cache_players_from_replays()
 
   averages = (average_rating(teams.get('mine')),
               average_rating(teams.get('theirs')))
