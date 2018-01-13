7c12b7f7e6465e4b9e551bb0cee707b42a7da1cd
Baldrickk <31375581+Baldrickk@users.noreply.github.com>
2018-01-13 19:56:06 +0000

    fixed some things.  one_line_string now works with "using" syntax
    something is broken still though, not sure what...

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from utils import one_line_string
+import itertools
 import json
 import requests
 
@@ -9,29 +10,30 @@ class API:
   def json_from_url(self, url):
     return json.loads(requests.get(url).text)
   
-def get_tank_tiers(self):
-  application_id = 'f9f772382b14bf0bad8b14d2d5dfd852'
-  url_str = 'https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id={}&fields=type,short_name,tier,tag&page_no={}'
-  page_number = 1
-  page_count = 1
-  tank_db = {}
-  while page_number <= page_count:
-    url = url_str.format(self.application_id, page_number)
-    json_data = json.loads(requests.get(url).text)
-    if not json_data.get('status') == 'ok':
-      break
-    page_count = json_data.get('meta').get('count')
-    page_number += 1
-    tank_dict = {tank_data.get('tag'):tank_data for tank_data in json_data.get('data').values()}
-    tank_db.update(tank_dict)
-  return tank_db
+  def get_tank_tiers(self):
+    ols = one_line_string()
+    url_str = 'https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id={}&fields=type,short_name,tier,tag&page_no={}'
+    page_number = 1
+    page_count = 1
+    tank_db = {}
+    while page_number <= page_count:
+      url = url_str.format(self.application_id, page_number)
+      json_data = json.loads(requests.get(url).text)
+      if not json_data.get('status') == 'ok':
+        break
+      page_count = json_data.get('meta').get('count')
+      ols.print(f'Getting tank tiers, page {page_number}/{page_count}')
+      page_number += 1
+      tank_dict = {tank_data.get('tag'):tank_data for tank_data in json_data.get('data').values()}
+      tank_db.update(tank_dict)
+    return tank_db
 
   def id_from_name(self, ols, idx, count, name):
-    ols.print(f'{idx}/{count}:{name}')
+    print(name)
+    ols.print(f'Getting player ID: {idx}/{count}:{name}')
     url = ('https://api.worldoftanks.eu/wot/account/list/?type=exact'
                              f'&application_id={self.application_id}'
                              f'&search={name}')
-    print (url)
     data = self.json_from_url(url)
     ok = (data.get('status') == 'ok' and 
           data.get('meta').get('count') > 0)
@@ -40,14 +42,29 @@ def get_tank_tiers(self):
   
   def ids_from_names(self, name_iter):
     print('Getting player IDs')
-    ols = one_line_string()
-    ids = [self.id_from_name(ols, idx, len(name_iter), name) 
-                        for idx, name in enumerate(name_iter)]
+    print(','.join(str(i)+str(v) for i, v in enumerate(name_iter)))
+    with one_line_string() as ols:
+      ids = (self.id_from_name(ols, idx+1, len(name_iter), name) 
+            for idx, name in enumerate(name_iter))
     return ids
-      
+    
+  def grouper(self, iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
+         
   def player_ratings_from_ids(self, id_iter):
     ratings = []
-    for i, group in enumerate(self.grouper(id_iter, 100)):
-      ids = ','.join(group)
+    for i, group in enumerate(self.grouper(id_iter, 2, '')):
+      players = {}
+      ids = ','.join(str(id) for id in group)
       url = f'https://api.worldoftanks.eu/wot/account/info/?application_id={self.application_id}&account_id={ids}&fields=global_rating,nickname'
       data = self.json_from_url(url)
+      if not data.get('status') == 'ok':
+        continue
+      else:
+        for player in data.get('data').values():
+          yield player
+
+
--- utils.py
+++ utils.py
@@ -4,7 +4,13 @@ class one_line_string:
   def __init__(self, output=sys.stdout):
     self.length = 0
     self.output = output
-  
+
+  def __enter__(self, output=sys.stdout):
+    return self
+    
+  def __exit__(self, type, value, traceback):
+    self.close()
+    
   def print(self, print_string):
     if len(print_string) > self.length:
       self.length = len(print_string)
@@ -12,3 +18,6 @@ class one_line_string:
           end = '', 
           file=self.output, 
           flush=True)
+
+  def close(self):
+    print ('\n')
