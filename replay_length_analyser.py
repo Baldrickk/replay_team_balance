df0b56acf1c4a7f1ea7c7c26aee515ab4f92feb8
Baldrickk <31375581+Baldrickk@users.noreply.github.com>
2018-01-26 22:56:01 +0000

    Fixed up replay_length_analyser to work with the updated replay analyser code

--- replay_length_analyser.py
+++ replay_length_analyser.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
 
-import requests
-import json
-import os
 import sys
-import itertools
+from utils import OverWriter as Ow
+from replay_parser import ReplayParser as Rp
+from statistics import mean
 
-from replay_analyser import load_json_from_replay
 
-def get_duration_from_replay(replay):
-  std, extended = load_json_from_replay(replay)
-  return std.get('common', {}).get('duration', None) if std else None
+def main():
+    if not len(sys.argv) > 1:
+        print('need a dir name')
+        exit()
 
-def main()
-  if not len(sys.argv) > 1:
-    print ('need a dir name')
-    exit()
+    with Ow(sys.stderr) as ow:
+        rp = Rp(sys.argv[1:], ow)
+        lengths = []
+        replays = rp.read_replays()
+        for replay in replays:
+            length = replay.get('ext', [{}])[0].get('common', {}).get('duration')
+            if length:
+                lengths.append(length)
+        average_length = mean(lengths)
+        minutes = int(average_length / 60)
+        seconds = str(int(average_length % 60)).rjust(2, '0')
+        print(f'\n{len(lengths)} replays:')
+        print(f'average duration: {minutes}:{seconds}')
 
-  dir = sys.argv[1].rstrip('/\\') + os.path.sep
-  files = list(os.listdir(dir))
-  total_length = 0
-  count = 0
-  max_str_len = 0
-  for i, replay in enumerate(files):
-    if 'wotreplay' not in replay:
-      continue
-    length = get_duration_from_replay(replay)
-    if length: 
-      total_length += length
-      count += 1
-  print (f'\n{count} replays:')
-  average_length = total_length / count
-  minutes = int(average_length / 60)
-  seconds = str(int(average_length % 60)).rjust(2,'0')
-  print (f'average duration: {minutes}:{seconds}')
 
 if __name__ == "__main__":
     main()
