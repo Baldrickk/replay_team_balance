8fb7accee36c0f885037c14e40314d48c6c86b64
ootjersb <33753713+ootjersb@users.noreply.github.com>
2018-02-01 15:12:37 +0100

    Bugfix too many replay file skipped (#4)
    * bugfix: domination is not only a Haloween mode. It is a normal game as well.
    
    * bugfix: domination is not only a Haloween mode. It is a normal game as well.
    
    * bugfix: detection for temp.wotreplay does not work when there is a directory name involved.
    
    * added: skip Halloween 2017 by mapname
    
    * changed: rsplit instead of endswith for temp.wotreplay exclusion

--- replay_parser.py
+++ replay_parser.py
@@ -37,9 +37,10 @@ class ReplayParser:
             if (len(json_data.get('vehicles')) < 30 or               # not full team
                     json_data.get('regionCode') == 'CT' or           # test server
                     json_data.get('bootcampCtx') or                  # tutorial
-                    json_data.get('gameplayID') in ['sandbox',       # proving grounds
-                                                    'domination']):  # halloween mode 2017
+                    json_data.get('gameplayID') == 'sandbox' or      # proving grounds
+                    json_data.get('mapName') == '120_kharkiv_halloween'):       # Halloween 2017
                 return None
+
             if parts == 2:
                 d = r.read(4)
                 data['ext'] = self._extract_json_data(d[0:4], r)
@@ -49,7 +50,7 @@ class ReplayParser:
         for directory in self.directorys:
             files = glob.glob(directory + os.path.sep + '*wotreplay')
             for i, replay in enumerate(files):
-                if replay in ['replay_last_battle.wotreplay', 'temp.wotreplay']:
+                if replay.rsplit (os.path.sep, 1)[-1] in ('replay_last_battle.wotreplay', 'temp.wotreplay'):
                     continue
                 self.ow.print(f'{i+1}/{len(files)} - {replay}')
                 json_data = self._load_json_from_replay(replay)
