f7b3ca3f2fc8e13f1aad1d22b7899a2a832f95c7
Baldrickk <31375581+Baldrickk@users.noreply.github.com>
2018-06-12 20:28:52 +0100

    SPEEEED! (thread pool) (#10)
    Changes to provide a thread pool to process IDs roughly 50x faster, and IDs roughly 5x faster (limited by API)
    Not at all inspired by my seeing Deadpool at the cinema today

--- api.py
+++ api.py
@@ -1,16 +1,37 @@
 import itertools
 import json
 import requests
-
+from random import randint
+from concurrent.futures import as_completed as as_comp
+from concurrent.futures import ThreadPoolExecutor as tpe
 
 class API:
     def __init__(self, application_id, over_writer):
         self.application_id = application_id
         self.ow = over_writer
 
-    @staticmethod
-    def json_from_url(url):
-        return json.loads(requests.get(url).text)
+    def requests_retry_session(self,
+      retries=10,
+      backoff_factor_min=10,
+      backoff_factor_max=20,
+      status_forcelist=(500, 502, 504),
+      session=None,
+  ):
+      session = session or requests.Session()
+      retry = requests.packages.urllib3.util.retry.Retry(
+          total=retries,
+          read=retries,
+          connect=retries,
+          backoff_factor=randint(backoff_factor_min, backoff_factor_max),
+          status_forcelist=status_forcelist,
+      )
+      adapter = requests.adapters.HTTPAdapter(max_retries=retry)
+      session.mount('http://', adapter)
+      session.mount('https://', adapter)
+      return session
+
+    def json_from_url(self, url):
+        return json.loads(self.requests_retry_session().get(url).text)
 
     def tank_tiers(self):
         with open('missingtanks.json') as missing:
@@ -23,7 +44,7 @@ class API:
         page_count = page_number + 1
         while page_number <= page_count:
             url = url_str.format(self.application_id, page_number)
-            json_data = json.loads(requests.get(url).text)
+            json_data = json.loads(self.requests_retry_session().get(url).text)
             if not json_data.get('status') == 'ok':
                 break
             page_count = json_data.get('meta').get('page_total')
@@ -35,6 +56,7 @@ class API:
         return tank_db
 
     def id_from_name(self, idx, count, name):
+        #print (f'{idx} - {count} - {name}')
         self.ow.print(f'Getting player ID: {idx}/{count}:{name}')
         url = ('https://api.worldoftanks.eu/wot/account/list/?type=exact'
                f'&application_id={self.application_id}'
@@ -50,9 +72,22 @@ class API:
             p_id = self.id_from_name(i+1, len(names), name)
             if p_id:
                 yield p_id
+                
+    def ids_from_names_generator_2(self, names):
+        with tpe() as executor:
+            future_to_id = {executor.submit(self.id_from_name, i+1, len(names), name): (i+1, name) for (i, name) in enumerate(names)}
+            for future in as_comp(future_to_id):
+                i, name = future_to_id[future]
+                try:
+                    id = future.result()
+                except Exception as exc:
+                    print(f'ID-ing {name} generated an exception: {exc}, retrying')
+                    #future_to_id[executor.submit(self.id_from_name, i, len(names), name)] = (i, name)
+                else:
+                    yield id
 
     def ids_from_names(self, name_iter):
-        return self.ids_from_names_generator(name_iter)
+        return self.ids_from_names_generator_2(name_iter)
 
     @staticmethod
     def grouper(iterable, n, fillvalue=None):
@@ -80,3 +115,33 @@ class API:
                         continue
                     player['id'] = player_id
                     yield player
+
+    def r_from_id_url(self, group):
+      url = ('https://api.worldoftanks.eu/wot/account/info/?'
+            f'application_id={self.application_id}&'
+            f'account_id={",".join(str(player_id) for player_id in group)}'
+             '&fields=global_rating,nickname')
+      return url
+    
+    def ratings_from_ids_2(self, id_iter):
+        with tpe() as executor:
+            future_to_rate = {executor.submit(self.json_from_url, self.r_from_id_url(group)): 
+                              (i+1, group) for (i, group) in enumerate(self.grouper(id_iter, 100, ''))}
+            for future in as_comp(future_to_rate):
+                i, group = future_to_rate[future]
+                self.ow.print(f'Getting Player ratings - {i*100}')
+                try:
+                    ratings = future.result()
+                    if not ratings.get('status') == 'ok':
+                        print(ratings)
+                except Exception as exc:
+                    print(f'rating-ing group {i} generated an exception: {exc}')
+                else:
+                    r_data = ratings.get('data')
+                    if r_data is not None:
+                      for player_id, player in r_data.items():
+                        if not player:
+                            continue
+                        player['id'] = player_id
+                        yield player
+
--- cache.py
+++ cache.py
@@ -9,15 +9,16 @@ class PlayerCache:
         self.key = field_names[0]
         self.field_names = field_names
         try:
-            with open(filename, 'r') as file:
-                for row in csv.DictReader(file, field_names):
+            with open(self.filename, 'r') as file:
+                for row in csv.DictReader(file, self.field_names):
                     self.add_to_cache(row)
         except FileNotFoundError:
             # there is no cache file, no problem.
             pass
-        self.cache_handle = open(filename, 'a', newline='')
-        self.writer = csv.DictWriter(self.cache_handle, field_names)
+        self.cache_handle = open(self.filename, 'a', newline='')
+        self.writer = csv.DictWriter(self.cache_handle, self.field_names)
         self.cache_handle.flush()
+        print(f'{len(self.data)} players loaded')
 
     def __enter__(self):
         return self
@@ -38,4 +39,10 @@ class PlayerCache:
 
     def close_cache(self):
         self.cache_handle.close()
+        self._write_clean_cache()
 
+    def _write_clean_cache(self):
+        with open(self.filename, 'w', newline='') as self.cache_handle:
+            self.writer = csv.DictWriter(self.cache_handle, self.field_names)
+            for record in self.data.items():
+                self.writer.writerow(record)
