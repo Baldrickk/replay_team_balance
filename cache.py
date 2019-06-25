6682ffd0c1099395520a2cec279fb75ec2704672
Edward Wright <ed.wright@live.co.uk>
2019-06-25 22:19:24 +0100

    Update cache.py
    black and pylint cache.py, 8.21 -> 10.0

--- cache.py
+++ cache.py
@@ -1,7 +1,10 @@
+"""TODO: Module Docstring"""
 import csv
 
 
 class PlayerCache:
+    """TODO: Class Docstring"""
+
     def __init__(self, filename, field_names):
         self.filename = filename
         self.data = {}
@@ -9,40 +12,46 @@ class PlayerCache:
         self.key = field_names[0]
         self.field_names = field_names
         try:
-            with open(self.filename, 'r') as file:
+            with open(self.filename, "r") as file:
                 for row in csv.DictReader(file, self.field_names):
                     self.add_to_cache(row)
         except FileNotFoundError:
             # there is no cache file, no problem.
             pass
-        self.cache_handle = open(self.filename, 'a', newline='')
+        self.cache_handle = open(self.filename, "a", newline="")
         self.writer = csv.DictWriter(self.cache_handle, self.field_names)
         self.cache_handle.flush()
-        print(f'{len(self.data)} players loaded')
+        print(f"{len(self.data)} players loaded")
 
     def __enter__(self):
+        """TODO: Method Docstring"""
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """TODO: Method Docstring"""
         self.close_cache()
 
     def add_to_cache(self, record):
-        existing = self.data.get(record.get(self.key),{})
-        a = all(str(existing.get(i)) == str(record.get(i)) for i in self.field_names)
-        if not a:
+        """TODO: Method Docstring"""
+        existing = self.data.get(record.get(self.key), {})
+        all_var = all(str(existing.get(i)) == str(record.get(i)) for i in self.field_names)
+        if not all_var:
             if self.cache_handle is not None:
                 self.writer.writerow(record)
             self.data[record.get(self.key)] = record
 
     def cached_record(self, name):
+        """TODO: Method Docstring"""
         return self.data.get(name)
 
     def close_cache(self):
+        """TODO: Method Docstring"""
         self.cache_handle.close()
         self._write_clean_cache()
 
     def _write_clean_cache(self):
-        with open(self.filename, 'w', newline='') as self.cache_handle:
+        """TODO: Method Docstring"""
+        with open(self.filename, "w", newline="") as self.cache_handle:
             self.writer = csv.DictWriter(self.cache_handle, self.field_names)
             for record in self.data.values():
                 self.writer.writerow(record)
