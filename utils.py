55d9c5c6514f344831fd26b2ebb554be28ae99fe
Edward Wright <ed.wright@live.co.uk>
2019-06-25 22:05:56 +0100

    Update utils.py
    used black on utils

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 """TODO: Module Docstring"""
 import sys
 
+
 class OverWriter:
     """TODO: Class Docstring"""
+
     def __init__(self, output=sys.stderr):
         """TODO: Method Docstring"""
         self.length = 0
@@ -19,9 +21,11 @@ class OverWriter:
     def print(self, print_string):
         """TODO: Method Docstring"""
         self.length = max(self.length, len(print_string))
-        print('\r' + print_string.ljust(self.length), end='', file=self.output, flush=True)
+        print(
+            "\r" + print_string.ljust(self.length), end="", file=self.output, flush=True
+        )
 
     @staticmethod
     def close():
         """TODO: Method Docstring"""
-        print('\n')
+        print("\n")
