15c52a8ec40d22db844c62a7a20846654cb058c8
Edward Wright <ed.wright@live.co.uk>
2019-06-25 21:57:35 +0100

    Update utils.py
    pylint utils.py 5.74 -> 9.29

--- utils.py
+++ utils.py
@@ -1,21 +1,27 @@
+"""TODO: Module Docstring"""
 import sys
 
-
 class OverWriter:
+    """TODO: Class Docstring"""
     def __init__(self, output=sys.stderr):
+        """TODO: Method Docstring"""
         self.length = 0
         self.output = output
 
     def __enter__(self):
+        """TODO: Method Docstring"""
         return self
-    
+
     def __exit__(self, type, value, traceback):
+        """TODO: Method Docstring"""
         self.close()
 
     def print(self, print_string):
+        """TODO: Method Docstring"""
         self.length = max(self.length, len(print_string))
         print('\r' + print_string.ljust(self.length), end='', file=self.output, flush=True)
 
     @staticmethod
     def close():
+        """TODO: Method Docstring"""
         print('\n')
